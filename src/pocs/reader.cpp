a2d832ee5fa45e04cc58252fc719fdae7cb5d7fd
Jorge Hevia <jmhev@outlook.com>
2020-04-20 23:58:00 +0200

    Fix memory leak in reader.cpp

--- src/pocs/reader.cpp
+++ src/pocs/reader.cpp
@@ -5,7 +5,9 @@
 int main(void)
 {
     config::Config_reader reader {};
-    config::config my_config = *reader.parse_config();
-    std::cout << my_config.toString() << std::endl;
+    config::config* my_config = reader.parse_config();
+    std::cout << my_config->toString() << std::endl;
+
+    delete my_config;
     return 0;
 }
